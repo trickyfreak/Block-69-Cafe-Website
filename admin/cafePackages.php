528fd42142987227e793a1356d3f090694abb3a6
pruvk <bannanashake1235@gmail.com>
2024-05-22 19:16:51 +0800

    started developing packages webpage (not yet done)

---
+++ admin/cafePackages.php
@@ -0,0 +1,57 @@
+<?php include('partials/header.php'); ?>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap" rel="stylesheet">
+<script src="https://cdn.tailwindcss.com"></script>
+
+<div class="flex flex-col gap-10">
+    <div class="bg-white text-center">
+        <h1 class="text-3xl mb-1">
+            Cafe Packages
+        </h1>
+        <h3 class="font-light">
+            Please note that there is a corkage fee of PHP 500 for each item brought into the venue. VAT inclusive.
+        </h3>
+    </div>
+    <div class="flex item-center justify-center">
+        <table class="table-fixed w-4/5 bg-white border-black border-2 text-center rounded-2xl">
+            <thead>
+                <tr class="">
+                    <th class="border-black border-2 p-10">Pax</th>
+                    <th class="border-black border-2 p-10">Rate</th>
+                    <th class="border-black border-2 p-10">Inclusion</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="border-black border-2 p-10">TBD</td>
+                    <td class="border-black border-2 p-10">TBD</td>
+                    <td class="border-black border-2 p-10">TBD</td>
+                </tr>
+                <tr>
+                    <td class="border-black border-2 p-10">TBD</td>
+                    <td class="border-black border-2 p-10">TBD</td>
+                    <td class="border-black border-2 p-10">TBD</td>
+                </tr>
+                <tr>
+                    <td class="border-black border-2 p-10">TBD</td>
+                    <td class="border-black border-2 p-10">TBD</td>
+                    <td class="border-black border-2 p-10">TBD</td>
+                </tr>
+                <tr>
+                    <td class="border-black border-2 p-10">TBD</td>
+                    <td class="border-black border-2 p-10">TBD</td>
+                    <td class="border-black border-2 p-10">TBD</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+
+
+
+
+</div>
+<?php include('partials/footer.php'); ?>
---
+++ admin/packages.php
@@ -0,0 +1,53 @@
+<?php include('partials/header.php'); ?>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap" rel="stylesheet">
+<script src="https://cdn.tailwindcss.com"></script>
+
+<section class="bg-white w-full h-full flex flex-col pb-24">
+    <div class="flex flex-col gap-7 text-center">
+        <div class="w-full h-96">
+            <img src="packagesImages/packageHome.png" alt="Home image" class="w-full h-96 object-cover">
+        </div>
+        <div class="px-40 gap-20">
+            <h1 class="text-3xl">
+                PACKAGES
+            </h1>
+            <h3 class="font-light">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            </h3>
+        </div>
+    </div>
+</section>
+
+<section class="flex justify-center w-full h-full flex pb-24">
+
+    <div class="flex flex-row w-4/5 gap-5">
+        <div class="flex flex-col w-auto h-auto border-black border-2 rounded-lg text-center items-center p-5">
+            <img src="packagesimages/coffee-icon.png" alt="" class="w-1/5 mb-5">
+            <h1 class="text-3xl mb-1">
+                Cafe Packages
+            </h1>
+            <h3 class="font-light">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </h3>
+            <a href="cafePackages.php" class="w-1/5 bg-white text-black border-black border-2 rounded p-3 mt-3 hover:bg-black hover:text-white">More</a>
+        </div>
+        <div class="flex flex-col w-auto h-auto border-black border-2 rounded-lg text-center items-center p-5">
+            <img src="packagesimages/chef-icon.png" alt="" class="w-1/5 mb-5">
+            <h1 class="text-3xl mb-1">
+                Catering Services
+            </h1>
+            <h3 class="font-light">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </h3>
+            <a href="cateringServices.php" class="w-1/5 bg-white text-black border-black border-2 rounded p-3 mt-3 hover:bg-black hover:text-white">More</a>
+        </div>
+    </div>
+
+</section>
+
+
+<?php include('partials/footer.php'); ?>
