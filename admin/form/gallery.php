192ce83c9a5418223266bd18decd5f77dd881845
xGihyun <giordnnuz@gmail.com>
2024-05-23 10:10:32 +0800

    Added dialog sample

---
+++ admin/form/gallery.php
@@ -0,0 +1,18 @@
+<?php
+$conn = get_connection();
+$img_id = $_POST['img_ID'];
+
+$file_name = $_FILES['file-img']['name'];
+$temp_name = $_FILES['file-img']['tmp_name'];
+$img_url = './Images/' . $file_name;
+move_uploaded_file($temp_name, $img_url);
+
+$conn->query("
+  UPDATE galleryimage 
+  SET img = '$img_url'
+  WHERE img_ID = '$img_id'
+");
+
+// Remove this
+echo "SUCCESSFULLY UPDATED";
+?>
