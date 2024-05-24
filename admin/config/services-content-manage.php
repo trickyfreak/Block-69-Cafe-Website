ecfbcc56d519366862f2a35f0349b8f5b9c0b94b
mnrdsnts <meinardsantoss@gmail.com>
2024-05-24 23:58:13 +0800

    completed adding content management system in services

--- admin/config/services-content-manage.php
+++ admin/config/services-content-manage.php
@@ -9,73 +9,24 @@ function get_services_content($conn) {
     return $all_content;
 }
 
-// Handle content addition/editing
-if (isset($_POST['submit'])) {
-    // Fetch Data
-    $content_id = mysqli_real_escape_string($conn, $_POST['content_id']);
-    $event_name = mysqli_real_escape_string($conn, $_POST['event_name']);
-    $content_title = mysqli_real_escape_string($conn, $_POST['content_title']);
-    $content_caption = mysqli_real_escape_string($conn, $_POST['content_caption']);
+// Handle content deletion
+if (isset($_POST['delete_content_id'])) {
+    $delete_content_id = mysqli_real_escape_string($conn, $_POST['delete_content_id']);
 
-    $uploaded_files = [];
-    $file_columns = ['content_image1', 'content_image2', 'content_image3', 'content_image4', 'content_image5'];
+     // Fetch the content to get the image paths
+     $query = "SELECT content_images FROM servicescontent WHERE content_id = '$delete_content_id'";
+     $result = mysqli_query($conn, $query);
+     $row = mysqli_fetch_assoc($result);
+     $imagePaths = explode(",", $row['content_images']);
+     
+     // Delete each image file
+     foreach ($imagePaths as $imagePath) {
+         if (file_exists($imagePath)) {
+             unlink($imagePath);
+         }
+     }
 
-    // Debugging: Print uploaded files
-    echo '<pre>';
-    print_r($_FILES['content_images']);
-    echo '</pre>';
-
-    // Handle file uploads
-    if (isset($_FILES['content_images']) && is_array($_FILES['content_images']['name']) && count($_FILES['content_images']['name']) > 0) {
-        for ($i = 0; $i < count($_FILES['content_images']['name']); $i++) {
-            if (!empty($_FILES['content_images']['name'][$i])) {
-                $file_name = $_FILES['content_images']['name'][$i];
-                $temp_name = $_FILES['content_images']['tmp_name'][$i];
-                $folder = './Events Images/' . $file_name;
-
-                // Debugging: Print file move status
-                if (move_uploaded_file($temp_name, $folder)) {
-                    $uploaded_files[] = $folder;
-                } else {
-                    echo "Failed to move uploaded file: $file_name<br>";
-                    echo "Temp file: $temp_name<br>";
-                    echo "Target directory: $folder<br>";
-                }
-            }
-        }
-    }
-
-    // Prepare columns and values for SQL query
-    $columns = ['event_name', 'content_title', 'content_caption'];
-    $values = ["'$event_name'", "'$content_title'", "'$content_caption'"];
-
-    for ($i = 0; $i < count($uploaded_files); $i++) {
-        if (isset($file_columns[$i])) {
-            $columns[] = $file_columns[$i];
-            $values[] = "'".$uploaded_files[$i]."'";
-        }
-    }
-
-    // Check Content Existence
-    $check_query = "SELECT * FROM servicescontent WHERE content_id = '$content_id'";
-    $check_result = mysqli_query($conn, $check_query);
-
-    if (mysqli_num_rows($check_result) > 0) {
-        // Update existing content
-        $update_values = [];
-        for ($i = 0; $i < count($columns); $i++) {
-            $update_values[] = $columns[$i] . " = " . $values[$i];
-        }
-        $query = "UPDATE servicescontent SET " . implode(', ', $update_values) . " WHERE content_id = '$content_id'";
-    } else {
-        // Insert new content
-        $query = "INSERT INTO servicescontent (" . implode(', ', $columns) . ") VALUES (" . implode(', ', $values) . ")";
-    }
-
-    // Debugging: Print query
-    echo $query . "<br>";
-
-    // Execute query
+    $query = "DELETE FROM servicescontent WHERE content_id = '$delete_content_id'";
     if (mysqli_query($conn, $query)) {
         echo '
         <script>
@@ -86,7 +37,97 @@ if (isset($_POST['submit'])) {
         </script>
         ';
     } else {
+        echo "Error deleting record: " . mysqli_error($conn);
+    }
+}
+
+// Handle content addition/editing
+if (isset($_POST['submit'])) {
+    // Fetch Data
+    $content_id = mysqli_real_escape_string($conn, $_POST['content_id']);
+    $event_name = mysqli_real_escape_string($conn, $_POST['event_name'.$content_id]);
+    $content_title = mysqli_real_escape_string($conn, $_POST['content_title'.$content_id]);
+    $content_caption = mysqli_real_escape_string($conn, $_POST['content_caption'.$content_id]);
+    $content_images_name = 'content_images';
+
+    // print_r($_FILES['content_images']);
+    $finalimage = array();
+
+    $extension = array('jpeg', 'jpg', 'png', 'gif');
+    if (!empty($_FILES[$content_images_name]['name'])) {
+        foreach ($_FILES[$content_images_name]['tmp_name'] as $key => $value) {
+            $file_name = $_FILES[$content_images_name]['name'][$key];
+            $temp_name = $_FILES[$content_images_name]['tmp_name'][$key];
+            $folder = './Events Images/' . $file_name;
+            $ext = pathinfo($file_name, PATHINFO_EXTENSION);
+
+            if (in_array($ext, $extension)){
+                if(!file_exists('./Events Images/'.$file_name)){
+                    move_uploaded_file($temp_name, $folder);
+                    $finalimage[]=$folder;
+                }else{
+                    $file_name = str_replace('.', '-', basename($file_name, $ext));
+                    $newfilename= $file_name.time().".".$ext;
+                    move_uploaded_file($temp_name, './Events Images/'.$newfilename);
+                    $finalimage[] = './Events Images/'.$newfilename;
+                }
+
+            }else{
+                // display error
+            }
+        }
+    }
+    // echo 'final image:';
+    // print_r($finalimage);
+    // Check Content Existence
+    $check_query = "SELECT * FROM servicescontent WHERE content_id = '$content_id'";
+    $check_result = mysqli_query($conn, $check_query);
+
+    if (mysqli_num_rows($check_result) > 0) {
+        // Update existing content
+        if (!empty($finalimage)) {
+            // Update with new image(s)
+            $image_paths = implode(",", $finalimage);
+            $query = "UPDATE servicescontent SET event_name = '$event_name', content_title = '$content_title', content_caption = '$content_caption', content_images = '$image_paths' WHERE content_id = '$content_id'";
+            
+            // Fetch the content to get the image paths
+            $query = "SELECT content_images FROM servicescontent WHERE content_id = '$content_id'";
+            $result = mysqli_query($conn, $query);
+            $row = mysqli_fetch_assoc($result);
+            $imagePaths = explode(",", $row['content_images']);
+            
+            // Delete each image file
+            foreach ($imagePaths as $imagePath) {
+                if (file_exists($imagePath)) {
+                    unlink($imagePath);
+                }
+            }
+
+        } else {
+            // Update without new image
+            $query = "UPDATE servicescontent SET event_name = '$event_name', content_title = '$content_title', content_caption = '$content_caption' WHERE content_id = '$content_id'";
+        }
+    } else {
+        // Insert new content
+        if (!empty($finalimage)) {
+            $image_paths = implode(",", $finalimage);
+            $query = "INSERT INTO servicescontent (content_id, event_name, content_title, content_caption, content_images) VALUES ('$content_id', '$event_name', '$content_title', '$content_caption', '$image_paths')";
+        } else {
+            $query = "INSERT INTO servicescontent (content_id, event_name, content_title, content_caption) VALUES ('$event_name', '$content_id', '$content_title', '$content_caption')";
+        }
+    }
+
+    if (mysqli_query($conn, $query)) {
+        echo '
+        <script>
+            if (window.history.replaceState) {
+                window.history.replaceState(null, null, window.location.href);
+            }
+        </script>
+        ';
+    } else {
         echo "Error: " . $query . "<br>" . mysqli_error($conn);
     }
 }
+
 ?>
--- admin/services.php
+++ admin/services.php
@@ -80,7 +80,7 @@ $content_id = 0;
             <textarea class="edit-content" name="content_caption"></textarea>
             <div>
             <label for="image" id="file-upload-label" class="custom-file-upload">Upload 5 images</label>
-            <input type="file" id="image" class="inputfile" name="content_images" multiple>
+            <input type="file" id="image" class="inputfile" name="content_images[]" multiple>
             </div>
             <input class="saveBtn" type="submit" name="submit">
         </div>
@@ -91,87 +91,70 @@ $content_id = 0;
     foreach ($content as $row) {
     $modalClass = 'modal-'.$row['content_id'];
     $contentIdName= 'content_id'.$row['content_id'];
+    $contentEventName = 'event_name'.$row['content_id'];
     $contentTitleName = 'content_title'.$row['content_id'];
     $contentCaptionName = 'content_caption'.$row['content_id'];
-    $image1 = 'content_image1'.$row['content_id'];
-    $image2 = 'content_image2'.$row['content_id'];
-    $image3 = 'content_image3'.$row['content_id'];
-    $image4 = 'content_image4'.$row['content_id'];
-    $image5 = 'content_image5'.$row['content_id'];
     $customFileUploadClass = 'custom-file-upload custom-file-upload'.$row['content_id'];
+    $contentImageId = 'content_images[]'.$row['content_id'];
+
+    echo '
+    <!-- Start of Edit Modal -->
+    <div class="bg-modal '.$modalClass.'">
+      <div class="modal-content">
+        <div class="close"><i class="fa-solid fa-square-xmark" style="color: black;"></i></div>
+            <p class="modal-title">EVENT NAME</p>
+            <textarea class="edit-content" name="'.$contentEventName.'">'.$row['event_name'].'</textarea>
+          <p class="modal-title">CONTENT TITLE</p>
+          <textarea class="edit-content" name="'.$contentTitleName.'">'.$row['content_title'].'</textarea>
+          <p class="modal-caption">CONTENT CAPTION</p>
+          <textarea class="edit-content" name="'.$contentCaptionName.'">'.$row['content_caption'].'</textarea>
+
+        <div>
+        <p style="color: white; margin:1em 2em 0em 2em; font-family: league spartan; font-size: 20px; font-weight:bold;">UPLOADED IMAGE</p>
+
+          <label for="image" class="custom-file-upload">Click to upload new 5 images</label>
+          <input type="file" id="image" name="content_images[]" class="inputfile" multiple>
+
+        </div>
+        <input class="saveBtn" type="submit" name="submit">
+      </div>
+    </div>
+    <!-- End of Modal -->';
 
     if($user_type == 'admin'){
-        // echo '
-        
-        // <div><button class="edit-btn" name="edit-btn" data-content-id="'.$row['content_id'].'" data-content-title="'. $row['content_title'].'" data-content-caption="'. $row['content_caption'].'" data-content-image="'.$row['content_image'].'">Edit Content <i class="fa-regular fa-pen-to-square"></i></button></div>
-        // ';
-    }
-        if($row['content_id'] > 0){
         echo '
-            <div class="events" style="margin-top: 2em;">
-            <div class="container_color">
-            <h1>'.$row['event_name'].'</h1>
-            <div class="container">
-                <div class="container_big_img">
-                    <img class="pic1" src="'.$row['content_image1'].'" alt="">
-                        <div class="container_small_img">
-                            <img class="img_throw1" src="'.$row['content_image2'].'" alt="">
-                            <img class="img_throw1"src="'.$row['content_image3'].'" alt="">
-                            <img class="img_throw1" src="'.$row['content_image4'].'" alt="">
-                            <img class="img_throw1" src="'.$row['content_image5'].'" alt="">
-                        </div>
-                </div>
-                <div class="container_text">
-                    <h4>'.$row['content_title'].'</h4>
-                    <p>'.$row['content_caption'].'</p>
-                </div>
-            </div>
-            </div>
+        
+        <div><button class="edit-btn" name="edit-btn" data-content-id="'.$row['content_id'].'" data-content-event="'.$row['event_name'].'" data-content-title="'. $row['content_title'].'" data-content-caption="'. $row['content_caption'].'" data-content-image="'.$row['content_images'].'">Edit Content <i class="fa-regular fa-pen-to-square"></i></button></div>
+        <button class="delete-btn" name="edit-btn" data-content-id="'.$row['content_id'].'">Delete Content <i class="fa-regular fa-trash-can"></i></button>
+
         ';
+    }
+        if ($row['content_id'] > 0) {
+            $imagePaths = explode(",", $row['content_images']);
+            echo '
+                <div class="events" style="margin-top: 2em;">
+                    <div class="container_color">
+                        <div class="eventName">'.$row['event_name'].'</div>
+                        <div class="container">
+                            <div class="container_big_img">
+                                <img class="pic1" src="'.$imagePaths[0].'" alt="">
+                                    <div class="container_small_img">
+                                        <img class="img_throw1" src="'.$imagePaths[1].'" alt="">
+                                        <img class="img_throw1" src="'.$imagePaths[2].'" alt="">
+                                        <img class="img_throw1" src="'.$imagePaths[3].'" alt="">
+                                        <img class="img_throw1" src="'.$imagePaths[4].'" alt="">
+                                    </div>
+                            </div>
+                            <div class="container_text">
+                                <h4>'.$row['content_title'].'</h4>
+                                <p>'.$row['content_caption'].'</p>
+                            </div>
+                        </div>
+                    </div>
+                ';
         }
     }
     ?>
-
-    <div class="container_color"">
-        <h1>corporate events</h1>
-      <div class="container">
-          <div class="container_big_img">
-              <img class="pic1" src="Events Images/14.png" alt="">
-                  <div class="container_small_img">
-                      <img class="img_throw1" src="Events Images/9.png" alt="">
-                      <img class="img_throw1" src="Events Images/10.png" alt="">
-                      <img class="img_throw1" src="Events Images/11.png" alt="">
-                      <img class="img_throw1" src="Events Images/13.png" alt="">
-                  </div>
-          </div>
-          <div class="container_text">
-              <h4>JAAM Foodcorp Christmas Party at Episode Bar + Kitchen</h4>
-              <p>150 cups served</p>
-          </div>
-      </div>
-      </div>
-
-    <div class="container_color" style="background-color: black;">
-        <!-- <div class="cont_h1"> -->
-            <h1 style="color: white;">community activity</h1>
-        <!-- </div> -->
-        <div class="container">
-            <div class="container_big_img">
-                    <div class="container_small_img_v2">
-                        <img class="img_throw1" src="Events Images/15.png" alt="">
-                        <img class="img_throw1"src="Events Images/20.jpg" alt="">
-                        <img class="img_throw1" src="Events Images/17.png" alt="">
-                        <img class="img_throw1"src="Events Images/18.png" alt="">
-                    </div>
-                    <img class="pic1" src="Events Images/19.png" alt="">
-            </div>
-            <div class="container_text">
-                <h4 style="color: white;">The Generics Pharmacy Medical Mission</h4>
-                <p style="color: white;">150 cups served</p>
-            </div>
-        </div>
-    </div>
-    </div>
     </form>
 
     <div class="announcement">
@@ -181,10 +164,18 @@ $content_id = 0;
 <?php include('partials/footer.php'); ?>
 
 <script>
+var modalClass = "modal-1";
+var contentTitle = "";
+var contentId = 1;
+var contentCaption = "";
+var contentImage ="";
+var contentImageId = 'content_images'+contentId;
+
 document.querySelectorAll('.edit-btn').forEach(function(button) {
     button.addEventListener('click', function(event) {
         event.preventDefault();
         //fetch data
+        eventName = button.getAttribute('data-content-event');
         contentTitle = button.getAttribute('data-content-title');
         contentCaption = button.getAttribute('data-content-caption');
         contentImage = button.getAttribute('data-content-image');
@@ -205,8 +196,6 @@ document.querySelectorAll('.add-btn').forEach(function(button) {
 
 //Function to handle edit button clicks
 function handleEditButtonClick(contentId) {
-  // console.log('contentID: ', contentId);
-  // console.log('content_id: ', content_id);
   modalClass = "modal-"+contentId;
   document.querySelector('input[name="content_id"]').value = contentId;
   document.querySelector('.bg-modal.'+modalClass).style.display = 'flex';
@@ -219,8 +208,28 @@ function handleAddButtonClick() {
 
 document.querySelectorAll('.close').forEach(function(element) {
   element.addEventListener('click', function() {
+    document.querySelector('.bg-modal').style.display = 'none';
     document.querySelector('.bg-modal-add').style.display = 'none';
   });
+});
+
+document.querySelectorAll('.delete-btn').forEach(function(button) {
+    button.addEventListener('click', function(event) {
+        event.preventDefault();
+        var contentId = button.getAttribute('data-content-id');
+        if (confirm('Are you sure you want to delete this content?')) {
+            var form = document.createElement('form');
+            form.method = 'post';
+            form.action = 'services.php';
+            var input = document.createElement('input');
+            input.type = 'hidden';
+            input.name = 'delete_content_id';
+            input.value = contentId;
+            form.appendChild(input);
+            document.body.appendChild(form);
+            form.submit();
+        }
+    });
 });
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -233,7 +242,7 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
-document.getElementById('content_image'+contentId).addEventListener('change', function() {
+document.getElementById('content_images[]'+contentId).addEventListener('change', function() {
   var fileName = this.files[0].name;
   var label = document.querySelector('.custom-file-upload.custom-file-upload'+contentId);
   label.textContent = fileName;
