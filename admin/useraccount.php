a3637c8122c4e3be5b639187f1d6c23402c17d40
John Lou <you@example.com>
2024-06-08 00:20:28 +0800

    added tracking of orders (incomplete)

--- admin/useraccount.php
+++ admin/useraccount.php
@@ -38,12 +38,12 @@ if(isset($_SESSION['username'])){
         </div>
         <div class="purchases">
             <div class="navigationbar">
-                <button>All</button>    
-                <button>To Ship</button>
-                <button>To Receive</button>
-                <button>Completed</button>
-                <button>Cancelled</button>
-                <button>Return Refund</button>
+                <button id="all" class="active">All</button>    
+                <button id="to-ship">To Ship</button>
+                <button id="to-receive">To Receive</button>
+                <button id="completed">Completed</button>
+                <button id="cancelled">Cancelled</button>
+                <button id="refund">Return Refund</button>
             </div>
             <?php 
             foreach($order_items as $orderInfo) {
@@ -54,6 +54,7 @@ if(isset($_SESSION['username'])){
                     <div>
                         <h1>'.$orderInfo['item_name'].'</h1>
                         <p>'.$orderInfo['item_category'].'</p>
+                        <p style="margin: 1em 0 0 0; font-size: 14px"> Quantity: '.$orderInfo['item_quantity'].'</p>
                     </div>
                 </div>
             </div>
@@ -91,3 +92,103 @@ if(isset($_SESSION['username'])){
         </div>
     </div>
 </div>
+
+<script>
+var buttons = document.querySelectorAll('.navigationbar button');
+
+buttons.forEach(function(button) {
+    button.addEventListener('click', function() {
+        buttons.forEach(function(btn) {
+            btn.classList.remove('active');
+        });
+
+        button.classList.add('active');
+
+        var summary = document.querySelectorAll('.all');
+        var toShip = document.querySelectorAll('.to-ship');
+        var toReceive = document.querySelectorAll('.to-receive');
+        var completed = document.querySelectorAll('.completed');
+        var cancelled = document.querySelectorAll('.cancelled');
+        var refund = document.querySelectorAll('.refund');
+        
+        if(button.id === '.all'){
+            summary.forEach(function(summary) {
+                summary.style.display = "flex";
+            });
+            toShip.forEach(function(toShip) {
+                toShip.style.display = "none";
+            });
+        }else if(button.id === 'to-ship') {
+            summary.forEach(function(summary) {
+                summary.style.display = "none";
+            });
+            toShip.forEach(function(toShip) {
+                toShip.style.display = "flex";
+            });
+        } else if(button.id === 'to-receive') {
+            summary.forEach(function(summary) {
+                summary.style.display = "none";
+            });
+            toShip.forEach(function(toShip) {
+                toShip.style.display = "none";
+            });
+            toReceive.forEach(function(toReceive) {
+                toShip.style.display = "flex";
+            });
+        } else if(button.id === 'completed') {
+            summary.forEach(function(summary) {
+                summary.style.display = "none";
+            });
+            toShip.forEach(function(toShip) {
+                toShip.style.display = "none";
+            });
+            toReceive.forEach(function(toReceive) {
+                toReceive.style.display = "none";
+            });
+            completed.forEach(function(completed) {
+                completed.style.display = "flex";
+            });
+        } else if(button.id === 'cancelled') {
+            summary.forEach(function(summary) {
+                summary.style.display = "none";
+            });
+            toShip.forEach(function(toShip) {
+                toShip.style.display = "none";
+            });
+            toReceive.forEach(function(toReceive) {
+                toReceive.style.display = "none";
+            });
+            completed.forEach(function(completed) {
+                completed.style.display = "none";
+            });
+            cancelled.forEach(function(cancelled) {
+                cancelled.style.display = "flex";
+            });
+        } else if(button.id === 'refund') {
+            summary.forEach(function(summary) {
+                summary.style.display = "none";
+            });
+            toShip.forEach(function(toShip) {
+                toShip.style.display = "none";
+            });
+            toReceive.forEach(function(toReceive) {
+                toReceive.style.display = "none";
+            });
+            completed.forEach(function(completed) {
+                completed.style.display = "none";
+            });
+            cancelled.forEach(function(cancelled) {
+                cancelled.style.display = "none";
+            });
+            refund.forEach(function(refund) {
+                refund.style.display = "flex";
+            });
+        } else {
+            var summary = document.querySelectorAll('.all');
+            summary.forEach(function(summary) {
+                summary.style.display = "block";
+            });
+        }
+    });
+});
+</script>
